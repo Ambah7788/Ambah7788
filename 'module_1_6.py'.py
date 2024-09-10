ddf5407cf4692dc1feea2ac55ab3b6d0d90bc613
Ambah7788 <akaevabdulkamal@gmail.com>
2024-09-10 23:00:02 +0300

    Add files via upload

---
+++ 'module_1_6.py'.py
@@ -0,0 +1,22 @@
+my_dict = {'Denis': 20, 'Ivan': 100,'Max': 1000}
+print(my_dict)
+my_dict.update({'Vika' : 1986, 'Gleb' : 1987})
+my_dict.pop('Gleb')
+print(my_dict)
+
+
+
+
+#Множество
+
+
+my_set = {1, 2, 3, 4, 5, 'String', True, (4, 3, 1, 2)}
+print(my_set)
+my_set.add(54)
+my_set.add(50)
+print(my_set )
+my_set.discard(54)
+print(my_set )
+
+
+
